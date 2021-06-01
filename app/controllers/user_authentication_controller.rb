c1ba6c2f6f987dc9dad612394345ecac255ef2c6
erikag213 <erika.gomez@chicagobooth.edu>
2021-06-01 15:15:14 +0000

    42%

--- app/controllers/user_authentication_controller.rb
+++ app/controllers/user_authentication_controller.rb
@@ -41,8 +41,6 @@ class UserAuthenticationController < ApplicationController
     @user.email = params.fetch("query_email")
     @user.password = params.fetch("query_password")
     @user.password_confirmation = params.fetch("query_password_confirmation")
-    @user.comments_count = params.fetch("query_comments_count")
-    @user.likes_count = params.fetch("query_likes_count")
     @user.private = params.fetch("query_private", false)
     @user.username = params.fetch("query_username")
 
