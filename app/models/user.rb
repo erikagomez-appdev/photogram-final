63a239c6909fcc52c2b12e4df5515e90a99b268c
erikag213 <erika.gomez@chicagobooth.edu>
2021-06-01 16:23:01 +0000

    users/[username] almost complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,4 +21,32 @@ class User < ApplicationRecord
     return Photo.where({ :owner_id => self.id })
   end
 
+  def sent_follow_requests
+    return FollowRequest.where({ :sender_id => self.id })
+  end
+
+  def received_follow_requests
+    return FollowRequest.where({ :recipient_id => self.id })
+  end
+
+  def accepted_sent_follow_requests
+    return self.sent_follow_requests.where({ :status => "accepted" })
+  end
+
+  def accepted_received_follow_requests
+    return self.received_follow_requests.where({ :status => "accepted" })
+  end
+
+  def followers
+    array_of_follower_ids = self.accepted_received_follow_requests.pluck(:sender_id)
+
+    return User.where({ :id => array_of_follower_ids })
+  end
+
+  def following
+    array_of_leader_ids = self.accepted_sent_follow_requests.pluck(:recipient_id)
+
+    return User.where({ :id => array_of_leader_ids })
+  end
+
 end
