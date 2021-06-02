20729a1e353cd17737436d54fea80e1f0818524f
erikag213 <erika.gomez@chicagobooth.edu>
2021-06-02 01:56:22 +0000

    feed works

--- app/models/user.rb
+++ app/models/user.rb
@@ -49,4 +49,10 @@ class User < ApplicationRecord
     return User.where({ :id => array_of_leader_ids })
   end
 
+  def feed
+    array_of_leader_ids = self.accepted_sent_follow_requests.pluck(:recipient_id)
+
+    return Photo.where({ :owner_id => array_of_leader_ids })
+  end
+
 end
